3fff6887d6b9dcdae69621b6168cb49ce4150d27
ggeorge <89857543+ggeorg0@users.noreply.github.com>
2023-06-02 15:37:56 +0300

    refactor(bot): take out all database code
    		take out all the code associated with the database
    		add global variable `db: Database` instead of

--- runbot.py
+++ runbot.py
@@ -6,9 +6,9 @@ from telegram import Update
 from telegram import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardRemove
 from telegram.ext import ApplicationBuilder, ContextTypes, filters
 from telegram.ext import CommandHandler, ConversationHandler, CallbackQueryHandler, MessageHandler, InvalidCallbackData
-import mysql.connector
-from mysql.connector import Error, errorcode
 import nltk
+
+from database import Database
 
 DB_CONFIG = {
     'host': '127.0.0.1',
@@ -67,85 +67,26 @@ UNKNOWN_COMMAND = "Неизвестная комманда. Помощь /help"
 LIST_H = 3
 MAX_BUTTON_CHARS = 50
 
+db: Database
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 
-def make_db_connection():
-    try:
-        return mysql.connector.connect(**DB_CONFIG)
-    except Error as err:
-        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-            logging.error('invalid login details')
-        elif err.errno == errorcode.ER_BAD_DB_ERROR:
-            logging.error('database does not exsist')
-        else:
-            logging.error(f'unexpected error: {err} in `make_db_connection`')
-        raise err
-
-def search_chat_status(chat_id: int, connection: mysql.connector.MySQLConnection):
-    """search chat status of `chat_id` in MySQL database.
-    If `chat_id` was not found, then a new entry is made to the database
-    
-    Return 'active', 'inactive' or `None`"""
-    try:
-        cursor = connection.cursor()
-        cursor.execute(f"SELECT COUNT(*) FROM chat WHERE id = {chat_id}")
-        entries_count = cursor.fetchone()[0]
-        if entries_count == 1:
-            cursor.execute(f"SELECT chat_status FROM chat WHERE id = {chat_id}")
-            return cursor.fetchone()[0]
-        return None
-    except Error as err:
-        if err.errno == errorcode.ER_PARSE_ERROR:
-            logging.error('incorrect SQL syntax in `search_chat_status`')
-        else:
-            logging.error(f'unexpected error: {err} in `search_chat_status`')      # TODO
-        raise err
-
-def record_new_chat(chat_id: int, connection: mysql.connector.MySQLConnection):
-    try:
-        cursor = connection.cursor()
-        cursor.execute(f"INSERT INTO chat (id, chat_status) VALUES ({chat_id}, 'active')")
-        connection.commit()
-    except Error as err:
-        if err.errno == errorcode.ER_PARSE_ERROR:
-            logging.error('incorrect SQL syntax in `record_new_chat`')
-        else:
-            logging.error(f'unexpected error: {err} in `record_new_chat`')      # TODO
-        raise err
-    
-def update_chat_status(chat_id: int, connection: mysql.connector.MySQLConnection):
-    try:
-        cursor = connection.cursor()
-        cursor.execute(f"UPDATE chat SET chat_status = 'active' WHERE id = {chat_id}")
-        connection.commit()
-    except Error as err:
-        if err.errno == errorcode.ER_PARSE_ERROR:
-            logging.error('incorrect SQL syntax in `update_chat_status`')
-        else:
-            logging.error(f'unexpected error: {err} in `update_chat_status`')      # TODO
-        raise err
-
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat_id = update.effective_chat.id
     remove_keyboard = ReplyKeyboardRemove()
-    try:
-        with make_db_connection() as connection:
-            status = search_chat_status(chat_id, connection)
-            if status == 'active':
-                await context.bot.send_message(chat_id, text=ACTIVE_START_MSG, reply_markup=remove_keyboard)
-            elif status == 'inactive':
-                update_chat_status(chat_id, connection)
-                await context.bot.send_message(chat_id, text=INACTIVE_START_MSG, reply_markup=remove_keyboard)
-            else:
-                record_new_chat(chat_id, connection)
-                await context.bot.send_message(chat_id, text=START_MSG, reply_markup=remove_keyboard)
-    except Error as err:
-        await context.bot.send_message(chat_id, text=ERR_MSG % err.errno)
-
+    status = db.chat_status(chat_id)
+    if status == 'active':
+        await context.bot.send_message(chat_id, text=ACTIVE_START_MSG, reply_markup=remove_keyboard)
+    elif status == 'inactive':
+        db.set_chat_active(chat_id)
+        await context.bot.send_message(chat_id, text=INACTIVE_START_MSG, reply_markup=remove_keyboard)
+    else:
+        db.record_new_chat(chat_id)
+        await context.bot.send_message(chat_id, text=START_MSG, reply_markup=remove_keyboard)
+    
 async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat_id = update.effective_chat.id
     await context.bot.send_message(chat_id, text=INFO_MSG, parse_mode='HTML')
@@ -153,7 +94,6 @@ async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def clear(update: Update, context: ContextTypes.DEFAULT_TYPE):
     reply_markup = ReplyKeyboardRemove()
     await context.bot.send_message(chat_id=update.effective_chat.id, text='')
-
 
 def add_switch_page_buttons(rows: int, desired_rows: int, page_num: int):
     if rows <= desired_rows and page_num == 1:
@@ -181,33 +121,17 @@ def build_books_menu(book_rows: list, desired_rows: int, page_num: int):
     buttons += add_switch_page_buttons(len(book_rows), desired_rows, page_num)
     return InlineKeyboardMarkup(buttons)
 
-def search_books(connection: mysql.connector.MySQLConnection, rows_count: int, offset: int = 0): # make connection argument first in other functions
-    try:
-        cursor = connection.cursor()
-        cursor.execute(f"SELECT id, title, author FROM book ORDER BY id LIMIT {offset}, {rows_count}")
-        return cursor.fetchall()
-    except Error as err:
-        if err.errno == errorcode.ER_PARSE_ERROR:
-            logging.error('incorrect SQL syntax in `update_chat_status`')
-        else:
-            logging.error(f'unexpected error: {err} in `update_chat_status`')      # TODO
-        raise err
-
 def make_books_page(max_rows: int, num: int):
-    with make_db_connection() as connection:
-        books = search_books(connection, 
-                            rows_count=max_rows+1,     # last row - indicator of additional data for page switch buttons
-                            offset=max_rows*(num-1)) 
+    # last row - indicator of additional data for page switch buttons
+    books = db.search_book(rows_count=max_rows+1, 
+                           offset=max_rows*(num-1))
     return build_books_menu(books, desired_rows=max_rows, page_num=num)
     
 async def show_first_page(update: Update, context: ContextTypes.DEFAULT_TYPE):
     logging.info('in `show_first_page`')
     chat_id = update.effective_chat.id
-    try:
-        choice_menu = make_books_page(LIST_H, num=1)
-        await context.bot.send_message(chat_id, "Выберите книгу", reply_markup=choice_menu)
-    except Error as err:
-        await context.bot.send_message(chat_id, ERR_MSG % err.errno)
+    choice_menu = make_books_page(LIST_H, num=1)
+    await context.bot.send_message(chat_id, "Выберите книгу", reply_markup=choice_menu)
     return "browse"
 
 async def switch_page(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -218,72 +142,23 @@ async def switch_page(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await update.callback_query.answer()
         return "browse"
     page_num = int(choice[5:])
-    try:
-        choice_menu = make_books_page(LIST_H, num=page_num)
-        await update.effective_message.edit_reply_markup(choice_menu)
-    except Error as err:
-        await context.bot.send_message(chat_id, ERR_MSG % err.errno)
+    choice_menu = make_books_page(LIST_H, num=page_num)
+    await update.effective_message.edit_reply_markup(choice_menu)
     await update.callback_query.answer()
     return "browse"
 
-
-def update_chat_book(connection: mysql.connector.MySQLConnection, chat_id: int, book_id: int):
-    try:
-        cursor = connection.cursor()
-        cursor.execute(f"UPDATE chat SET book_id = {book_id} WHERE id = {chat_id}")
-        connection.commit()
-    except Error as err:
-        if err.errno == errorcode.ER_PARSE_ERROR:
-            logging.error('incorrect SQL syntax in `update_chat_book`')
-        elif err.errno == errorcode.ER_NO_REFERENCED_ROW_2:
-            logging.error('book id foreign key constraint fails in `update_chat_book`')
-        else:
-            logging.error(f'unexpected error: {err} in `update_chat_book`')
-        raise err
-    
-def search_book_info(connection: mysql.connector.MySQLConnection, book_id: int):
-    try:
-        cursor = connection.cursor()
-        cursor.execute(f"SELECT title, author, info FROM book WHERE id = {book_id}")
-        return cursor.fetchone()
-    except Error as err:
-        if err.errno == errorcode.ER_PARSE_ERROR:
-            logging.error('incorrect SQL syntax in `search_book_info`')
-        else:
-            logging.error(f'unexpected error: {err} in `search_book_info`')
-        raise err
-
 def gather_summary_message(title: str, author: str, info: str):
     message = "Вы выбрали: <b>{0}</b>\nАвторы: {1}\nОписание: {2}\nВыбрать другую книгу /book"
-    if info:
+    if info != 'NULL':
         message =  message.format(title, author, info)
     message = message.format(title, author, 'нет описания')
     return message
 
-def search_max_page(connection: mysql.connector.MySQLConnection, chat_id: int):
-    try:
-        cursor = connection.cursor()
-        cursor.execute(f"SELECT MAX(num) FROM page WHERE book_id = \
-                        (SELECT book_id FROM chat WHERE id = {chat_id})")
-        return cursor.fetchone()[0]
-    except Error as err:
-        if err.errno == errorcode.ER_PARSE_ERROR:
-            logging.error(f'incorrect SQL syntax in `search_max_page`')
-        else:
-            logging.error(f'unexpected error: {err} in `search_max_page`')
-        raise err
-
 def gather_maxpage_message(chat_id: int):
-    try:
-        with make_db_connection() as connection:
-            max_page = search_max_page(connection, chat_id)
-    except Error as err:
-        # we send message without telling client that something has gone wrong 
-        # in case it's temporary failure in database 
-        # and we can continue using mysql connector later
-        return SELECT_PAGE_MSG 
+    max_page = db.search_max_page(chat_id)
+    if max_page == None:
+        return SELECT_PAGE_MSG
     return SELECT_PAGE_MSG + MAX_PAGE_PHRASE % max_page
-
 
 async def set_book(update: Update, context: ContextTypes.DEFAULT_TYPE):
     choice = update.callback_query.data
@@ -292,81 +167,54 @@ async def set_book(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.callback_query.answer()
     try:
         book_id = int(choice[5:])
-        with make_db_connection() as connection:
-            update_chat_book(connection, chat_id, book_id)
-            book_info = search_book_info(connection, book_id)
     except ValueError:                      # prevent sql injection
         await update.effective_message.edit_text(ERR_VALUE_MSG)
-    except Error as err:
-        await context.bot.send_message(chat_id, ERR_MSG % err.errno)
     else:
+        db.update_chat_book(chat_id, book_id)
+        book_info = db.book_metadata(book_id)
         await update.effective_message.edit_text(gather_summary_message(*book_info),
                                                  parse_mode='HTML')
         await asyncio.sleep(0.5)
         await context.bot.send_message(chat_id, text=gather_maxpage_message(chat_id), 
                                        parse_mode='HTML')
-
     return ConversationHandler.END
-
-def search_page_content(connection: mysql.connector.MySQLConnection, chat_id: int, page: int):
-    try:
-        cursor = connection.cursor()
-        cursor.execute(f"SELECT content FROM page WHERE \
-                       num = {page} AND \
-                       book_id = (SELECT book_id FROM chat WHERE id = {chat_id})")
-        return nltk.tokenize.sent_tokenize(cursor.fetchone()[0], language='russian')
-    except Error as err:
-        if err.errno == errorcode.ER_PARSE_ERROR:
-            logging.error(f'incorrect SQL syntax in `search_page_content`')
-        else:
-            logging.error(f'unexpected error: {err} in `search_page_content`')
-        raise err
 
 async def select_page(update: Update, context: ContextTypes.DEFAULT_TYPE):
     logging.info('in `select_page`')
     chat_id = update.effective_chat.id
     selected_page = int(update.message.text)
-    try:
-        with make_db_connection() as connection:
-            max_page = search_max_page(connection, chat_id)
-            if max_page == None:
-                raise TypeError
-            if selected_page < 1 or selected_page > max_page:
-                raise ValueError
-            max_sent = len(search_page_content(connection, chat_id, selected_page))
-    except (Error, ValueError, TypeError) as ex:
-        if isinstance(ex, Error):
-            await context.bot.send_message(chat_id, ERR_MSG % ex.errno)
-        if isinstance(ex, TypeError):
-            await context.bot.send_message(chat_id, )
-        if isinstance(ex, ValueError):
-            await context.bot.send_message(chat_id, ERR_SELECT_PAGE_MSG + MAX_PAGE_PHRASE % max_page,
-                                           parse_mode='HTML')
+    max_page = db.search_max_page(chat_id)
+    if max_page == None:
+        await context.bot.send_message(chat_id, ERR_SELECT_PAGE_MSG)
+        return ConversationHandler.END 
+    if selected_page < 1 or selected_page > max_page:
+        message = ERR_SELECT_PAGE_MSG + MAX_PAGE_PHRASE % max_page
+        await context.bot.send_message(chat_id, message, parse_mode='HTML')
         return ConversationHandler.END
+    page_text = db.page_content(chat_id, selected_page)
+    # TODO: save tokenize result (sentences) to cache 
+    sentences = nltk.tokenize.sent_tokenize(page_text, language='russian')
     context.chat_data[chat_id] = selected_page
-    await context.bot.send_message(chat_id, SELECT_SENT_MSG + MAX_SENT_PHRASE % max_sent,
-                                   parse_mode='HTML')
+    message = SELECT_SENT_MSG + MAX_SENT_PHRASE % len(sentences)
+    await context.bot.send_message(chat_id, message, parse_mode='HTML')
     return "browse"
 
 async def page_line(update: Update, context: ContextTypes.DEFAULT_TYPE):
     logging.info('int `page_line`')
     chat_id = update.effective_chat.id
-    sent_num = int(update.message.text)
     try:
         page_num = context.chat_data[chat_id]
-        with make_db_connection() as connection:
-            sents = search_page_content(connection, chat_id, page_num)
+        page_text = db.page_content(chat_id, page_num)
+        sents = nltk.tokenize.sent_tokenize(page_text, language='russian')
+        sent_num = int(update.message.text)
         if sent_num < 1 or sent_num > len(sents):
             raise ValueError
     except KeyError:
         await context.bot.send_message(chat_id, ERR_NO_PAGE)
     except ValueError:
-        await context.bot.send_message(chat_id, 
-                                       ERR_SELECT_SENT_MSG + MAX_SENT_PHRASE % len(sents),
-                                       parse_mode='HTML')
+        message = ERR_SELECT_SENT_MSG + MAX_SENT_PHRASE % len(sents)
+        await context.bot.send_message(chat_id, message, parse_mode='HTML')
         return "browse"
-    except Error as err:
-        await context.bot.send_message(chat_id, ERR_MSG % err.errno)
     else:
         await context.bot.send_message(chat_id, VERIFY_MSG % (sent_num, page_num))
         await asyncio.sleep(1)
@@ -393,7 +241,7 @@ async def default_error(update: Update, context: ContextTypes.DEFAULT_TYPE):
     return "browse"
 
 async def command_not_found(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    logging.info('in ``command_not_found')
+    logging.info('in `command_not_found`')
     chat_id = update.effective_chat.id
     await context.bot.send_message(chat_id, UNKNOWN_COMMAND)
 
@@ -403,7 +251,7 @@ async def handle_invalid_button(update: Update, context: ContextTypes.DEFAULT_TY
     await update.callback_query.answer()
     await update.effective_message.edit_text(INVALID_BUTTON_MSG)
 
-def main():
+def run_bot():
     application = ApplicationBuilder().token(os.environ.get('TOKEN')).build()
 
     start_handler = CommandHandler('start', start)
@@ -444,6 +292,6 @@ def main():
 
     application.run_polling()
 
-
 if __name__ == '__main__':
-    main()
+    db = Database(DB_CONFIG)
+    run_bot()
