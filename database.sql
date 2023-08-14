da9c85fb17738220907e425187395d4897802272
ggeorge <89857543+ggeorg0@users.noreply.github.com>
2023-08-14 11:49:34 +0300

    fix(db-creation): table names and start timestamp
    		  `role expiration` will be executed every day at 3 am

--- database.sql
+++ database.sql
@@ -1,11 +1,13 @@
 -- old `chat`, `page`, `book` tables here
 
--- new `role` and `user_role` tables here
+-- new `role` and `chat_role` tables here
 
 -- create scheduler to track expiration of user roles
 CREATE EVENT role_expiration
     ON SCHEDULE
         EVERY 1 DAY
+        -- event starts tomorrow at 3 am
+        STARTS CURDATE() + INTERVAL 1 DAY + INTERVAL 3 HOUR
     COMMENT 'Check expiration date of user role every day'
     DO
-        DELETE FROM user_role WHERE CURDATE() > user_role.expiration;
+        DELETE FROM chat_role WHERE CURDATE() > chat_role.expiration;
