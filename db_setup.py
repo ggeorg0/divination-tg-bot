3516ea7958b4ee6dfee16e4289cc0490f36f8c0e
ggeorge <89857543+ggeorg0@users.noreply.github.com>
2023-08-17 11:41:04 +0300

    chore: clean up (comments and etc.)

--- db_setup.py
+++ db_setup.py
@@ -5,9 +5,7 @@ import sys
 import mysql.connector
 from mysql.connector import Error, errorcode
 
-from schema import TABLES, CREATE_STATEMENTS
-
-DB_NAME = 'test_bot_db'
+from schema import CREATE_STATEMENTS
 
 logging.basicConfig(level=logging.INFO)
 
@@ -28,36 +26,6 @@ def establish_connection():
             raise err
         sys.exit(-1)
     return cnx
-
-# def create_database(db_name, cursor, character_set='utf8mb4'):
-#     logging.debug(f"creating database '`{db_name}`'...")
-#     try:
-#         cursor.execute(f"CREATE DATABASE `{db_name}` DEFAULT CHARACTER SET {character_set}")
-#     except Error as err:
-#         if err.errno == errorcode.ER_DB_CREATE_EXISTS:
-#             logging.warning(f"database `{db_name}` exists")
-#         else:
-#             raise err
-
-# def use_database(db_name, cursor):
-#     logging.debug(f"trying to select `{db_name}` as default schema...")
-#     try:
-#         cursor.execute(f"USE `{db_name}`")
-#     except Error as err:
-#         if err.errno == errorcode.ER_BAD_DB_ERROR:
-#             logging.error(f"database `{db_name}` does not exists")
-#         raise err
-        
-# def create_tables(tables, cursor):
-#     for t in tables:
-#         try:
-#             logging.debug(f"creating table `{t}`...")
-#             cursor.execute(tables[t])
-#         except Error as err:
-#             if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
-#                 logging.warning(f"table `{t}` already exists")
-#             else:
-#                 raise err
 
 def setup_database():
     with establish_connection() as connection:
--- schema.py
+++ schema.py
@@ -4,38 +4,6 @@ import sys
 WARN_MSG = """built-in dict class has the ability to remember insertion order in Python 3.7 or above, 
 your Python version is %s.%s.%s. It means the table creation order may be incorrect."""
 
-TABLES = {}
-
-TABLES['book'] = """
-CREATE TABLE book(
-    id SMALLINT UNSIGNED NOT NULL,
-    title VARCHAR(1024),
-    author VARCHAR(1024),
-    info VARCHAR(2048),
-    PRIMARY KEY (id)
-)
-"""
-       
-TABLES['chat'] = """
-CREATE TABLE chat(
-    id BIGINT UNSIGNED PRIMARY KEY,
-    chat_status ENUM('active', 'inactive') NOT NULL,
-    rights ENUM('user', 'admin') NOT NULL DEFAULT 'user',
-    book_id SMALLINT UNSIGNED NULL,
-    FOREIGN KEY (book_id) 
-        REFERENCES book(id)
-)"""
-       
-TABLES['page'] = """
-CREATE TABLE page(
-    book_id SMALLINT UNSIGNED,
-    num MEDIUMINT UNSIGNED,
-    content VARCHAR(4096),
-    PRIMARY KEY (book_id, num),
-    FOREIGN KEY (book_id)
-        REFERENCES book (id)
-)"""
-
 CREATE_STATEMENTS = {}
 
 CREATE_STATEMENTS['schema'] = """
