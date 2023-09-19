0dd58d557ffdcf2c670e83db92ac531bd7b2dc79
ggeorge <89857543+ggeorg0@users.noreply.github.com>
2023-09-19 11:30:12 +0300

    feat(db-setup): insert first admin on db creation

--- db_setup.py
+++ db_setup.py
@@ -5,7 +5,11 @@ import sys
 import mysql.connector
 from mysql.connector import Error, errorcode
 
-from schema import CREATE_STATEMENTS, INSERT_STATEMENTS
+from schema import (
+    TABLES_CREATION, 
+    ROLE_INSERTIONS,
+    ADMIN_INSERTION
+)
 
 logging.basicConfig(level=logging.INFO)
 
@@ -15,7 +19,8 @@ def establish_connection():
         cnx = mysql.connector.connect(
             host='127.0.0.1',
             user=os.environ.get('DB_USER'),
-            password=os.environ.get('DB_PASS'))
+            password=os.environ.get('DB_PASS'),
+            autocommit=True)
         logging.info('connection with mysql server is established')
     except Error as err:
         if err.errno == errorcode.CR_CONN_HOST_ERROR:
@@ -28,22 +33,33 @@ def establish_connection():
     return cnx
 
 def setup_schema(connection):
-    for alias, statement in CREATE_STATEMENTS.items():
+    for alias, statement in TABLES_CREATION.items():
         with connection.cursor() as curs:
             logging.info(f'create stage: {alias}')
             curs.execute(statement)
-    connection.commit()
 
 def insert_roles(connection):
-    for alisas, insertion in INSERT_STATEMENTS.items():
+    for alisas, insertion in ROLE_INSERTIONS.items():
         with connection.cursor() as curs:
             logging.info(f"insertion stage: {alisas}")
             curs.execute(insertion)
-    connection.commit()
-        
+
+def insert_admin(connection):
+    admin_id = input("\nEnter id of the first admin (or press Enter for skip): ")
+    if admin_id.strip():
+        logging.info(f"inserting admin with id {admin_id}")
+        for alias, statement in ADMIN_INSERTION.items():
+            with connection.cursor() as curs:
+                logging.info(f"admin insertion: {alias}")
+                curs.execute(statement.format(admin_id))
+    else:
+        logging.info("skiping admin insertion")
+
 
 if __name__ == '__main__':
     logging.getLogger(mysql.connector.__name__).setLevel(logging.WARNING)
     with establish_connection() as connection:
         setup_schema(connection)
         insert_roles(connection)
+        insert_admin(connection)
+        logging.info("Done.")
